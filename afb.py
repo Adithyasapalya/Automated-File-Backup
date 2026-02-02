ee0ae14d2fd4d8839d66ce0fd712e7b4633cf700
Adithya Sapalya <adithyasapalya30@gmail.com>
2026-02-02 22:52:53 +0530

    added source and dest

--- afb.py
+++ afb.py
@@ -4,4 +4,7 @@ import datetime
 import schedule
 import time
 
-source_dir = "D:\Adi\Code\Github\Automated File Backup"
+source_dir = "C:\Users\abhij\OneDrive\Pictures\Screenshots"
+des_dir = "D:\Adi\Code\Github\Automated File Backup\Destination Dir"
+
+
