8bd22fcc6404c135bc63a244c2b1e229e41dc340
Adithya Sapalya <adithyasapalya30@gmail.com>
2026-02-02 22:57:21 +0530

    try and expect added

--- afb.py
+++ afb.py
@@ -4,7 +4,15 @@ import datetime
 import schedule
 import time
 
-source_dir = "C:\Users\abhij\OneDrive\Pictures\Screenshots"
-des_dir = "D:\Adi\Code\Github\Automated File Backup\Destination Dir"
+source_dir = "C:\Users\abhi\\OneDriv\\Picture\\Screenshots"
+destination_dir = "D:\Ad\\Cod\\Githu\\Automated File Backup\Destination Dir"
 
+def copy_folder_to_dir(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, today.str(today))
 
+    try :
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied successfully to {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in {dest_dir}")
